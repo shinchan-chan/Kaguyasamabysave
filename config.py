105e3c6efcc059a227c33a66e695fc082efbed31
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-03-22 21:25:17 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
 PORT = os.environ.get("PORT", "8080")
 
 #Database --⚠️ REQUIRED--
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sanji01:sanji01@sanjimongo02.zgs4i.mongodb.net/?retryWrites=true&w=KaguyaShinomiyaFileBot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sanji01:sanji01@sanjimongo02.zgs4i.mongodb.net/?retryWrites=true&w=KaguyaSama")
 DB_NAME = os.environ.get("DATABASE_NAME", "KaguyaSama")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
