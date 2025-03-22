60b4573c2278da819c3975b9065c1e30876e1a9c
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-03-22 21:30:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "")
 PORT = os.environ.get("PORT", "8080")
 
 #Database --⚠️ REQUIRED--
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sanji01:sanji01@sanjimongo02.zgs4i.mongodb.net/?retryWrites=true&w=KaguyaSama")
-DB_NAME = os.environ.get("DATABASE_NAME", "KaguyaSama")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://sanji01:sanji01@sanjimongo02.zgs4i.mongodb.net/?retryWrites=true&w=KaguyaShinomiyaFileBot")
+DB_NAME = os.environ.get("DATABASE_NAME", "Kaguya_Shinomiya_File_Bot")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
